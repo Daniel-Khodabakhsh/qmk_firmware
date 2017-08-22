830bce64d66da07afa5652fbc0c6fe6095d17c94
Daniel Khodabakhsh <d.khodabakhsh@gmail.com>
2017-08-21 18:05:38 -0700

    s60: add volume control

--- keyboards/s60_x/keymaps/dan/keymap.c
+++ keyboards/s60_x/keymaps/dan/keymap.c
@@ -30,7 +30,7 @@ enum macros {
 	MACRO_LOCK_OS
 };
 
-#define M_LOCK M(MACRO_LOCK_OS)
+#define M_OSLCK M(0)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Main layer
@@ -60,15 +60,15 @@ KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                   
 ├────────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴────────┤
 │          │LEFT │DOWN │RIGHT│PGDN │     │     │     │     │LockO│     │     │            │
 ├──────────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴────────────┤
-│            │     │     │     │     │     │     │     │     │     │     │                │
+│            │     │     │     │     │     │     │Mute │VolDn│VolUp│     │                │
 ├──────┬─────┴┬────┴─┬───┴─────┴─────┴─────┴─────┴─────┴─────┴┬────┴─┬───┴──┬──────┬──────┤
 │      │      │      │                                        │      │      │      │      │
 └──────┴──────┴──────┴────────────────────────────────────────┴──────┴──────┴──────┴──────┘*/
 	[LAYER_FUNC] = KEYMAP(
 KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_DEL,\
 _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, KC_INS,  _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,\
-_______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, M_LOCK,  _______, _______, XXXXXXX, _______,\
-_______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, _______, XXXXXXX,\
+_______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, M_OSLCK, _______, _______, XXXXXXX, _______,\
+_______, XXXXXXX, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, XXXXXXX, _______, XXXXXXX,\
 _______, _______, _______,                            _______,                            _______, _______, _______, _______)
 };
 
@@ -84,8 +84,9 @@ _______, _______, _______,                            _______,                  
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 	if (record->event.pressed) {
 		switch(id) {
-			case MACRO_LOCK_OS:
-				return MACRO(D(LGUI), T(L), U(LGUI), END);
+			case 0:
+				return MACRO(D(LSFT), T(H), U(LSFT), T(I), D(LSFT), T(1), U(LSFT), END);
+				//return MACRO(D(LGUI), T(L), U(LGUI), END);
 		}
 	}
 	
